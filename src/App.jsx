6bc504c85ec216702515d372789bb6675c21305c
Raju lal <147190023+Rajulal-195@users.noreply.github.com>
2025-06-05 12:54:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Addproject from "./Addproject"
+import Displayitem from './Displayitem'
+export default function App() {
+  return (
+    <div>
+        {/* <div className="text-center bg-gradient-to-r from-red-400 via-yellow-300 to-green-400 h-64 w-full">hello</div> */}
+<Addproject/>
+{/* <Displayitem/> */}
+
+    </div>
+  )
+}
